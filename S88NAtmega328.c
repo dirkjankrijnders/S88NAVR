46a24e58d97876076debc17b4071e5c8b9128ff3
Dirkjan Krijnders <dirkjankrijnders@incas3.eu>
2013-02-18 16:17:05 +0100

    - #def'd the S88 pins

--- S88NAtmega328.c
+++ S88NAtmega328.c
@@ -1,5 +1,10 @@
 #define F_CPU 8000000UL
-#define LOAD INT1
+
+#define S88LOAD PD3 // INT1
+#define S88CLK PD2
+#define S88RST // Unused
+#define S88DATAIN PB7
+#define S88DATAOUT PD0
 
 #include <inttypes.h>
 #include <avr/io.h>
@@ -45,8 +50,8 @@ int main(){
   EIMSK |= (1 << INT1);     // Turns on INT1
   sei();
   while(1) {
-    PORTB ^= (1 << 5);
-  };
+//    PORTB ^= (1 << 5);
+//  };
   return 0;
 };
 

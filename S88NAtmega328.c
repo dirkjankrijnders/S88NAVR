0719c7acde5ff5ef8f8de33134df531470e57758
Dirkjan Krijnders <dirkjankrijnders@incas3.eu>
2013-02-18 16:30:40 +0100

    - Cleanup\r- Used pin definitions

--- S88NAtmega328.c
+++ S88NAtmega328.c
@@ -1,4 +1,4 @@
-#define F_CPU 8000000UL
+//#define F_CPU 8000000UL
 
 #define S88LOAD PD3 // INT1
 #define S88CLK PD2
@@ -17,16 +17,16 @@ uint8_t m = 0;
 
 ISR(INT0_vect) {
   cli();
-  if (((PIND & 0b00000100) == 0b00000100)) {   // If rising edge, write S88DATAOUT
+  if (((PIND & (1 << S88CLK)) == (1 << S88CLK))) {   // If rising edge, write S88DATAOUT
     if ((buffer & 1) == 1) {
-      PORTD |= 0b11111111; //(1 << 6);
+      PORTD |= (1 << S88DATAOUT); //(1 << 6);
     } else {
-      PORTD &= 0b11011111;
+      PORTD &= ~(1 << S88DATAOUT);
     }
     // Shift register and add value;
     buffer = (buffer >> 1);// | t;
   } else {   // If falling edge, read S88DATAIN
-    t = (PIND & 16) ? msb : 0;
+    t = (PINB & S88DATAIN) ? msb : 0;
     buffer |= (t << 15);
   };
   
@@ -40,9 +40,10 @@ ISR(INT1_vect) { // LOAD
 }
 
 int main(){
-  DDRB = (1 << 5);
-  DDRC = 0;
-  DDRD = 0b00100010; // Arduino Pro Mini TX and pin 6 as output 
+  DDRD |= (1 << S88DATAOUT); // Is the only output...
+//  DDRB = (1 << 5);
+//  DDRC = 0;
+//  DDRD = 0b00100010; // Arduino Pro Mini TX and pin 6 as output 
   EICRA |= (1 << ISC00);    // set INT0 to trigger on ANY logical change
   EIMSK |= (1 << INT0);     // Turns on INT0
   EICRA |= (1 << ISC10);    // set INT1 to trigger rising edge
@@ -51,7 +52,7 @@ int main(){
   sei();
   while(1) {
 //    PORTB ^= (1 << 5);
-//  };
+  };
   return 0;
 };
 
